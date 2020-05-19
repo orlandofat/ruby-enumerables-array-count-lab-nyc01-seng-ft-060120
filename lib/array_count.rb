922246e31034d4ed1fc35a9215796d4e9eeee711
Orlando Challenger <orlandofat@gmail.com>
2020-05-19 21:05:05 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,7 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
 
-  array.count{|s| s.class == String && element.empty?}
+  array.count{|s| s.class == String}
 end
 
 def count_empty_strings(array)
